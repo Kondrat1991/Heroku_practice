25016d92d47da28c0db87f1281d7d68b806e8d26
Kondrat1991 <kondratmychailo1991@gmail.com>
2018-06-27 10:48:21 +0300

    remove authenticate

--- api/routes/users.js
+++ api/routes/users.js
@@ -82,7 +82,7 @@ router.post('/login', function (req, res, next) {
 
 router.get('/login/facebook', passport.authenticate('facebook') );
 
-router.get('/login/facebook/callback', passport.authenticate('facebook'), (req,res) => {res.send("You are welcome at Facebook!!!")} );
+router.get('/login/facebook/callback', (req,res) => {res.send("You are welcome at Facebook!!!")} );
 
 router.get('/login/google', passport.authenticate('google',{scope:['profile']}));
 
