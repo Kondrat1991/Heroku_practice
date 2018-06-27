ceadf1d83aeba3f4e4011fd5e19beb06e38616d6
Kondrat1991 <kondratmychailo1991@gmail.com>
2018-06-27 11:09:24 +0300

    next

--- api/routes/loginByFacebook.js
+++ api/routes/loginByFacebook.js
@@ -2,28 +2,34 @@ const mongoose = require('mongoose');
 const express = require('express');
 const passport = require('passport');
 const FacebookStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/users');
+// const User = require('../models/users');
+const Social = require('../models/socialUser');
 
   let userFacebook = () => {
       passport.use(new FacebookStrategy({
               clientID: 1993107304353343,
               clientSecret: '4ca499c5cac0921d44b08b288685c118',
-              callbackURL: "http://secret-falls-75483.herokuapp.com/users/login/facebook/callback"
+              callbackURL: "https://secret-falls-75483.herokuapp.com/users/login/facebook/callback"
           },
           (req, accessToken, refreshToken, profile, done) => {
               console.log('check profile', profile);
               let data = profile._json;
-              User.findOne({email: data.email})
+              Social.findOne({id: data.id})
                   .exec()
                   .then(user => {
                       if (!user) {
-                          const user = new User({
-                              email: data.email,
-                          })
+                          const newUser = new Social({
+                              id: data.id,
+                              displayName: data.displayName,
+                              provider: data.provider
+                          });
+                          newUser.save();
+                          done(null, newUser);
                       }
+
                   })
                   .catch(err => {
-                      res.status(500).json(err);
+                      console.log(err);
                   })
           }
       ));
